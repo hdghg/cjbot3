2a4507d15f8fd1bd38a803de2278955e4768681b
Alexey Tokar <tokar.a.v@yandex.ru>
2021-10-23 14:36:23 +0300

    Refactor search module to log failed search queries

--- src/main/java/com/gitlab/hdghg/cjbot3/module/bing/BingSearchModule.java
+++ src/main/java/com/gitlab/hdghg/cjbot3/module/bing/BingSearchModule.java
@@ -6,9 +6,13 @@ import com.gitlab.hdghg.cjbot3.model.bing.Value;
 import com.gitlab.hdghg.cjbot3.module.Module;
 import com.pengrad.telegrambot.model.Message;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class BingSearchModule implements Module {
+
+    private static final Logger log = Logger.getLogger(BingSearchModule.class.getName());
 
     private final SearchClient search;
     private final String subscriptionKey;
@@ -28,17 +32,30 @@ public class BingSearchModule implements Module {
             return Optional.empty();
         }
         String query = text.substring(3);
-        SearchResult searchResults;
-        try {
-            searchResults = search.searchWeb(subscriptionKey, query);
-        } catch (Exception e) {
+        SearchResult searchResult = search.searchWeb(subscriptionKey, query);
+        return valueFromResult(searchResult, query)
+                .map(v -> new ChatMessage(message.chat().id(), v, message.messageId()));
+    }
+
+    Optional<String> valueFromResult(SearchResult searchResult, String query) {
+        if (null == searchResult) {
+            log.warning("null searchResult for query: '" + query + "'");
             return Optional.empty();
         }
-        Value value = searchResults.getWebPages().getValue().get(0);
-        String url = value.getUrl();
-        String name = value.getName();
-        String snippet = value.getSnippet();
-        String result = name + "\n" + snippet + "\n" + url;
-        return Optional.of(new ChatMessage(message.chat().id(), result, message.messageId()));
+        if (null == searchResult.getWebPages()) {
+            log.warning("SearchResult has no 'webPages' for query: '" + query + "'");
+            return Optional.empty();
+        }
+        List<Value> valueList = searchResult.getWebPages().getValue();
+        if (null == valueList) {
+            log.warning("SearchResult.webPages has no 'value' for query: '" + query + "'");
+            return Optional.empty();
+        }
+        if (0 == valueList.size()) {
+            log.warning("SearchResult.webPages.value is empty for query: '" + query + "'");
+            return Optional.empty();
+        }
+        return Optional.of(valueList.get(0))
+                .map(v -> v.getUrl() + "\n" + v.getName() + "\n" + v.getSnippet());
     }
 }
